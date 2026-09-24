ebd4d3eb7fd9d4385d2b84bbd06c075804804c51
mmatt16 <79289663+mmatt16@users.noreply.github.com>
2026-09-24 15:44:28 -0400

    Adjust Header styles for responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -61,8 +61,9 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow: auto;
+  
     @media ${QUERIES.tabletAndSmaller} {
     justify-content: space-between;
     align-items: center;
@@ -77,7 +78,7 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
 
     @media ${QUERIES.tabletAndSmaller} {
